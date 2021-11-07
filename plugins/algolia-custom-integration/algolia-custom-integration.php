cac89039066d085a19693a968806739191b792b7
JamesVanWaza <jgacuca@gmail.com>
2021-11-07 16:28:33 -0500

    Create algolia-custom-integration.php

---
+++ plugins/algolia-custom-integration/algolia-custom-integration.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name:     Algolia Custom Integration
+ * Description:     Add Algolia Search feature
+ * Text Domain:     algolia-custom-integration
+ * Version:         1.0.0
+ *
+ * @package         Algolia_Custom_Integration
+ */
+
+// Your code starts here.
