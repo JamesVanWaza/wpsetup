55f51c2f094ea6f9022ee55aa0d0a8efae91f3d5
JamesVanWaza <jgacuca@gmail.com>
2021-11-07 16:31:09 -0500

    Updated require vendor

--- plugins/algolia-custom-integration/algolia-custom-integration.php
+++ plugins/algolia-custom-integration/algolia-custom-integration.php
@@ -9,4 +9,9 @@
  * @package         Algolia_Custom_Integration
  */
 
-// Your code starts here.
+require_once __DIR__ . '/vendor/autoload.php';
+
+global $algolia;
+
+$algolia = \Algolia\AlgoliaSearch\SearchClient::create("INSERTID", "INSERTAPIKEY");
+
